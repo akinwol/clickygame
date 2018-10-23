a0496507d1c7f7dbc467b4b055f53c9615484553
Nyra Dev <37726406+akinwol@users.noreply.github.com>
2018-10-23 18:08:01 -0400

    v1.2

---
+++ clickygame/client/build/precache-manifest.856c6ca34c19e91de8aafcee0c1e7af7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66e5cb78dfe30d20990b",
+    "url": "/static/js/main.66e5cb78.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "66e5cb78dfe30d20990b",
+    "url": "/static/css/main.99a6ad94.chunk.css"
+  },
+  {
+    "revision": "9e69fe2a6459f728c69564462ad7a533",
+    "url": "/index.html"
+  }
+];
